6b382168fa02dc90e5e7155000d2dd540e2f8de7
ShukranBabaev <marina300@bk.ru>
2022-05-01 13:14:19 +0300

    Diplom

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,29 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         // создаём конфиг
         LinksSuggester linksSuggester = new LinksSuggester(new File("data/config"));
+
+        var dir = new File("data/pdfs");
+        for (var fileIn : dir.listFiles()) {
+            System.out.println(fileIn);
+        }
+
+
+        var fileIn = new File("data/pdfs/Проба.pdf");
+        var fileOut = new File("data/pdfs/Проба.pdf");
+        var doc = new PdfDocument(new PdfReader(new FileInputStream(fileIn)), new PdfWriter(new FileOutputStream(fileOut)));
+        var page1 = doc.getPage(1);
+        var text = PdfTextExtractor.getTextFromPage(page1);
+        System.out.println(text);
 
         // перебираем пдфки в data/pdfs
 
